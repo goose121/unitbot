e9d7ab61c27ff45a6c7c33dee22ac3a45467833a
goose121 <goose121@users.noreply.github.com>
2018-01-27 23:21:10 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,68 @@
+package main;
+
+import (
+    "os"
+    "flag"
+	"syscall"
+	"os/signal"
+	"fmt"
+	"strings"
+    "github.com/bwmarrin/discordgo"
+)
+
+func init() {
+    flag.StringVar(&token, "t", "", "Bot Token")
+    flag.Parse()
+}
+
+var token string
+
+func main() {
+    if token == "" {
+        os.Exit(1)
+    }
+
+    dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Running! Interrupt to exit")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if strings.HasPrefix(m.Content, ".convert") || strings.HasPrefix(m.Content, ".c") {
+		conversion := strings.TrimPrefix(strings.TrimPrefix(m.Content, ".convert "), ".c ")
+		var quant float64;
+		var unit1, unit2 string;
+		fmt.Sscanf(conversion, "%f %s to %s", &quant, &unit1, &unit2)
+
+		var out, err = ConvertVal(quant, strings.ToLower(strings.TrimSuffix(unit1, "s")), strings.ToLower(strings.TrimSuffix(unit2, "s")))
+		if err != nil {
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Conversion error: %v", err))
+			return
+		}
+
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%f %s is %f %s", quant, unit1, out, unit2))
+	}
+}
---
+++ units.go
@@ -0,0 +1,134 @@
+package main;
+
+import (
+	"math"
+	"strings"
+	"errors"
+	"fmt"
+)
+
+type Unit struct {
+    // The scale factor from the base metric unit
+    // to this unit
+    scaleFactor float64
+    // The offset from metric to this unit
+    // mostly for temperature units
+    unitOffset float64
+}
+
+var Distances = map[string]Unit{
+	"metre": {1.0, 0.0},
+	"meter": {1.0, 0.0},
+	"foot":  {0.3048, 0.0},
+	"feet":  {0.3048, 0.0},
+	"inch":  {0.0254, 0.0},
+	"inche": {0.0254, 0.0},
+	"yard":  {0.9144, 0.0},
+	"mile":  {1609.344, 0.0},
+}
+
+var Weights = map[string]Unit{
+	"gram": {1.0, 0.0},
+	"ounce": {28.34952, 0.0},
+	"pound": {453.5924, 0.0},
+}
+
+var Temps = map[string]Unit {
+	"celsiu": {1.0, 0.0},
+	"fahrenheit": {0.5555555555555, 32.0},
+	"kelvin": {1.0, -273.15},
+}
+
+var SiPrefixes = map[string]int{
+    "exa": 18,
+    "peta": 15,
+    "tera": 12,
+    "giga": 9,
+    "mega": 6,
+    "kilo": 3,
+    "hecto": 2,
+    "deca": 1,
+    "deci": -1,
+    "centi": -2,
+    "milli": -3,
+    "micro": -6,
+    "nano": -9,
+    "pico": -12,
+    "femto": -15,
+    "atto": -18,
+}
+
+var ShortForms = map[string]string {
+	"cm": "centimeter",
+	"m": "meter",
+	"ft": "foot",
+	"yd": "yard",
+	"mi": "mile",
+	"km": "kilometer",
+	"c": "celsiu",
+	"f": "fahrenheit",
+	"k": "kelvin",
+	"mg": "milligram",
+	"g": "gram",
+	"kg": "kilogram",
+	"oz": "ounce",
+	"lb": "pound",
+}
+
+var Categories = []map[string]Unit {Distances, Weights, Temps}
+
+func ConvertVal(val float64, unit1 string, unit2 string) (float64, error) {
+	unit1Ex, unit2Ex := unit1, unit2
+	
+	if ShortForms[unit1] != "" {
+		unit1Ex = ShortForms[unit1]
+	}
+
+	if ShortForms[unit2] != "" {
+		unit2Ex = ShortForms[unit2]
+	}
+	
+	base1Str, scale1 := parsePrefixes(unit1Ex)
+	base2Str, scale2 := parsePrefixes(unit2Ex)
+
+	var base1, base2 Unit;
+	
+	catloop:
+	for _, cat := range Categories {
+		isCat := false
+		for unitStr := range cat {
+			if base1Str == unitStr {
+				base1 = cat[base1Str]
+				isCat = true
+			}
+			if base2Str == unitStr {
+				base2 = cat[base2Str]
+				isCat = true
+			}
+		}
+		if isCat {
+			break catloop
+		}
+	}
+
+	if(base1.scaleFactor == 0.0) {
+		return 0.0, errors.New(fmt.Sprintf("invalid unit '%v'", unit1))
+	}
+
+	if(base2.scaleFactor == 0.0) {
+		return 0.0, errors.New(fmt.Sprintf("invalid unit '%v'", unit2))
+	}
+
+	return (val - base1.unitOffset) * ((base1.scaleFactor * scale1) / (base2.scaleFactor * scale2)) + base2.unitOffset, nil
+}
+
+// takes in a prefixed unit, outputs the name without prefixes
+// and the scale factor specified by the prefixes
+func parsePrefixes(unit string) (string, float64) {
+	for k := range SiPrefixes {
+		if strings.HasPrefix(unit, k) {
+			return strings.TrimPrefix(unit, k), math.Pow10(SiPrefixes[k])
+		}
+	}
+	return unit, 1.0
+}
